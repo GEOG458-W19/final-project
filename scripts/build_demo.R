83a165d57c3d294780019fd78d962771a4718ffc
ginue0206 <46503546+ginue0206@users.noreply.github.com>
2019-03-13 09:16:16 -0700

    added updated demo map

--- scripts/build_demo.R
+++ scripts/build_demo.R
@@ -13,17 +13,6 @@ calls <- readOGR("./data/2018_Fire_Calls_Seattle/2018_Fire_Calls_Seattle.shp",
 calls_df <- st_read("./data/2018_Fire_Calls_Seattle/2018_Fire_Calls_Seattle.shp")
 pop <- st_read("./data/population_data_shp/population_data/population.shp")
 
-#counts the number of times each call occurs and selects the top 10. add desc() to arrange to go form most calls to lowest
-pop_grouped <- pop %>% group_by(TRACTCE10) %>% summarize(pop=sum(POP10)) %>% arrange(pop)
-top_10_call_types <- calls_df %>% group_by(Type) %>% summarize(total_numb_calls=n()) %>% arrange(desc(total_numb_calls)) %>% head(10) 
-call_top_10_df <- filter(calls_df, Type %in% unique(top_10_call_types$Type))
-
-
-#assigns different colors to different categories according to domain. unique() returns 
-#list of unique values in the calls shp in the type column
-colors <- c("#AD1E1E","#F18932","#FFD916","#C7F715","#070406","#6CCCE2","#0F3ECA","#390FCC","#8E01EE","#F5008B")
-palette_fn <- colorFactor(colors, call_top_10_df$Type)
-palette_pop <- colorNumeric("Blues", pop_grouped$pop)
 seattle_18 <- st_read("./data/2018_Fire_Calls_Seattle/2018_Fire_Calls_Seattle.shp")
 seattle_17 <- st_read("./data/2017_Fire_Calls_Seattle/2017_Fire_Calls_Seattle.shp")
 
@@ -64,7 +53,24 @@ demo_map <- function(year) {
               opacity = 1
     )
   # creates map
-  map <- leaflet(data = call_top_10_df) %>% 
+map <- function(year){
+  # Determines which year of data set to display
+  yr_df <- ""
+  if (year == 18) {
+    yr_df <- seattle_18
+  } else {
+    yr_df <- seattle_17 
+  } 
+  #counts the number of times each call occurs and selects the top 10. add desc() to arrange to go form most calls to lowest
+  pop_grouped <- pop %>% group_by(TRACTCE10) %>% summarize(pop=sum(POP10)) %>% arrange(pop)
+  top_10_call_types <- yr_df %>% group_by(Type) %>% summarize(total_numb_calls=n()) %>% arrange(desc(total_numb_calls)) %>% head(10) 
+  call_top_10_df <- filter(yr_df, Type %in% c("Medic Response","Trans to AMR", "Auto Fire Alarm"))
+  #assigns different colors to different categories according to domain. unique() returns 
+  #list of unique values in the calls shp in the type column
+  colors <- c("#AD1E1E","#F18932","#FFD916","#C7F715","#070406","#6CCCE2","#0F3ECA","#390FCC","#8E01EE","#F5008B")
+  palette_fn <- colorFactor(colors, call_top_10_df$Type)
+  palette_pop <- colorNumeric("Blues", pop_grouped$pop)
+  leaflet(data = call_top_10_df) %>% 
     addTiles() %>% 
     setView(lng = -122.32945, lat = 47.60357, zoom = 12) %>%
     #adds population density polygons
@@ -84,6 +90,8 @@ demo_map <- function(year) {
     ) %>%
     addLegend("bottomright",pal=palette_fn,values=~Type,
               title = "Call Types", opacity=1) 
+
+}
   
   return(p)
 }
